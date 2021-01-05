ac709a1bf29ce08f575e82240267f3786a71205f
Isabella Guimet <isabellaguimet@hotmail.com>
2021-01-05 16:25:39 -0700

    Script results can now be seen in a text file

--- datesMaker.py
+++ datesMaker.py
@@ -7,8 +7,8 @@ from datetime import timedelta
 # Used these links for resources: https://www.w3schools.com/python/python_datetime.asp
 # http://www.pressthered.com/adding_dates_and_times_in_python/
 
-# Date of first Monday of the first week of the semester. For Fall 2020: Sept 7th
-date = datetime.datetime(2020, 9, 7)
+# Date of first Monday of the first week of the semester. For Winter 2021: January 11th
+date = datetime.datetime(2021, 1, 11)
 
 # Month name - Short version: ex: Sep
 abbrevMonth = date.strftime("%b")
@@ -17,6 +17,11 @@ abbrevMonth = date.strftime("%b")
 abbrevDay = date.strftime("%d")
 
 i = 1
+
+# Open file to store dates in, "w" stands for overwrite
+datesStoredFile = open("unidatesText.txt", "w")
+
+datesStoredFile.write("Here are the dates for the Winter 2021 Semester! \n\n")
 
 while (i < 15):
     if (abbrevDay == "01" or abbrevDay == "21" or abbrevDay == "31"):
@@ -28,6 +33,10 @@ while (i < 15):
     else:
         prefix = "th"
 
+    # Writes all the dates for all the weeks in the semester to a .txt file
+    datesStoredFile.write("Week " + str(i) + ": " + abbrevMonth + ". " + abbrevDay + prefix + "\n")
+
+    # Write all the dates for all the weeks in the semester to the command line interface
     print("Week " + str(i) + ": " + abbrevMonth + ". " + abbrevDay + prefix)
 
     # adds 7 days (1 week) to the current date
@@ -36,3 +45,6 @@ while (i < 15):
     abbrevMonth = date.strftime("%b")
     abbrevDay = date.strftime("%d")
     i = i + 1
+
+# closes the unused file
+datesStoredFile.close()
